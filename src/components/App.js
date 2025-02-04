c794aa32f3ebb0c8d04b227ac8fed2d21029a3c6
louis-ngugi <louisngugi858@gmail.com>
2025-02-04 14:20:18 +0300

    Imported React and useState for managing state in the NewPlantForm component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) => setPlants(data));
+  }, []);
+
   return (
     <div className="app">
+      {plants.map((plant) => (
+        <div key={plant.id}>
+          <h2>{plant.name}</h2>
+          <img src={plant.image} alt={plant.name} width="150" />
+          <p>${plant.price.toFixed(2)}</p>
+        </div>
+      ))}
       <Header />
       <PlantPage />
     </div>
